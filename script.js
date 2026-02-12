c42f98edcc0899701a262bf58eb0298ef98e10f5
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-12 12:21:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
         let radius = prompt("Enter the radius of the circle:");
         // write you code here and display the result to the user
         let area = 3.14 * radius * radius
-		console("The area of the circle with radius 4 is ", area.toFixed(2) )
+		return "The area of the circle with radius 4 is ", area.toFixed(2) 
 }
 calculateArea();
